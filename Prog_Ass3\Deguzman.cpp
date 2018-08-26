bf46a097756c12a506c9509829e37a8e3dc87fe8
deguzmanaileen <42116874+deguzmanaileen@users.noreply.github.com>
2018-08-27 01:08:29 +0800

    Add files via upload

---
+++ Prog_Ass3\Deguzman.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rabbit,disease;
+    cout<<"There were";
+    cin>>rabbit;
+    cout<<"rabbits in the farm.\n";
+    cout<<"\n\n";
+    cout<<"But there happened a disease and the farm lost";
+    cin>>disease;
+    cout<<"rabbits in the farm";
+    cout<<"\n\n";
+    cout<<"\n\nIf the farm lost how many rabbits were there left?n\n";
+    cout<< "The answer is=";
+    cout<<rabbit-disease;
+    cout<<"\n\n";
+    cout<<("PAUSE");
+    
+    return 0;
+    
+    }
